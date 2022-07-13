c3a60941c8a0d2b0a09c13f8f5541e6fc268a516
seois830926 <idmahoro@gmail.com>
2022-07-13 17:10:18 +0900

    addTodo method 작성

---
+++ components/AddTodo.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-container>
+        <v-row>
+          <v-col>
+              <v-text-field v-model="input_todo" label="할일"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-menu
+              v-model="menu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="date"
+                  label="날짜선택"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="date"
+                @input="menu = false"
+              ></v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col>
+            <v-btn
+              class="mx-2"
+              fab
+              dark
+              color="indigo"
+              @click="addTodo"
+            >
+              <v-icon dark>
+                mdi-plus
+              </v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+  name: 'AddTodoList',
+  data: () => ({
+    input_todo: '',
+    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    menu: false,
+  }),
+  methods: {
+    addTodo () {
+        /**
+         * 1. add시 TodoList 페이지로 값전달
+         * 2. 전달 받은 값 post
+         * 3. 이상없이 갔으면 전달받은 값 state에 추가
+         */
+        alert(123);
+        const { input_todo, date } = this;
+        console.log(input_todo, date);
+    }
+  }
+}
+</script>
+
--- pages/TodoList.vue
+++ pages/TodoList.vue
@@ -5,58 +5,15 @@
                 <h1> TodoList Page </h1>
             </v-col>
         </v-row>
-        <v-row>
-          <v-col>
-              <v-text-field v-model="input_todo" label="할일"></v-text-field>
-          </v-col>
-          <v-col>
-            <v-menu
-              v-model="menu"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-              min-width="auto"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                  v-model="date"
-                  label="날짜선택"
-                  prepend-icon="mdi-calendar"
-                  readonly
-                  v-bind="attrs"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                v-model="date"
-                @input="menu = false"
-              ></v-date-picker>
-            </v-menu>
-          </v-col>
-          <v-col>
-            <v-btn
-              class="mx-2"
-              fab
-              dark
-              color="indigo"
-            >
-              <v-icon dark>
-                mdi-plus
-              </v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
+        <add-todo></add-todo>
     </v-container>
 </template>
 
 <script>
+import AddTodo from '../components/AddTodo.vue'
 export default {
+  components: { AddTodo },
   name: 'TodoListPage',
-  data: () => ({
-    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-    menu: false,
-  }),
 }
 </script>
 
